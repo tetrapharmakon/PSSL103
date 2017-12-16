a6118bac48f16204e555a6a04aa5295ce84cbf2c
Paolo Brasolin <>
2017-12-16 22:48:51 +0100

    stub submissions

---
+++ submissions/create.php
@@ -0,0 +1,37 @@
+<?php
+
+error_reporting(E_ALL ^ E_WARNING); 
+
+// if(empty($_POST['token']) || $_POST['token'] !== $_SESSION['token']){
+//   exit("Bad token!");
+// }
+// unset($_SESSION['token']);
+
+// var_dump($_POST);
+
+$serialized_data = json_encode($_POST);
+$notifications_recipient = 'paolo.brasolin@gmail.com';
+
+try {
+  // Write the contents to the file,
+  // using the FILE_APPEND flag to append the content to the end of the file
+  // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+  $success = file_put_contents('list.json', $serialized_data.PHP_EOL, FILE_APPEND | LOCK_EX);
+
+  // file_put_contents raises no exceptions on failure but it returns false
+  if($success){
+    $mail_subject = "Successful submission";
+    mail($notifications_recipient, $mail_subject, $serialized_data);
+    header("Location: success.php");
+    die();
+  } else {
+    throw new Exception("file_put_contents unsuccessful");
+  }
+} catch (Exception $e) {
+    $mail_subject = "Errorful submission: ".($e->getMessage());
+    mail($notifications_recipient, $mail_subject, $serialized_data);
+    header("Location: failure.php");
+    die();
+}
+
+?>
